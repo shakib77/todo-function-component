4d13498ddf8c7128bfa36e4202f2adc670c79343
shakib77 <shakibshaon77@gmail.com>
2021-09-29 19:36:27 +0600

    working on todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import {useEffect, useState} from "react";
 import './App.css';
 import TodoItems from "./components/TodoItems";
 
+// import AddItems from "./components/AddItems"
+
 function App() {
+
+    const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        let response = await (
+            await fetch("https://jsonplaceholder.typicode.com/todos/")
+        ).json();
+        console.log('res->', response);
+        setTodo(response);
+    };
+
     return (
         <>
-            <div className="todo_title">
-                <h3>Todo List</h3>
-            </div>
-            <TodoItems />
+            <h1>Todo list</h1>
+            <TodoItems todo={todo}/>
         </>
     );
 }
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,139 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 
-const getLocalStorageItems = () => {
-    const myList = localStorage.getItem("lists");
-    if (myList) {
-        return JSON.parse(localStorage.getItem("lists"));
-    } else {
-        return []
-    }
-};
 
-const TodoItems = () => {
-    const [input, setInput] = useState('');
-    const [items, setItems] = useState(getLocalStorageItems());
-    const [toggleSubmit, setToggleSubmit] = useState(true);
-    const [isEdit, setIsEdit] = useState(null);
+import {DataGrid, GridRowsProp, GridColDef} from '@mui/x-data-grid';
+import {Button} from "@mui/material";
 
-    let createRef = React.createRef();
+/*const deleteItem = (id) => {
+    const filterItems = items.filter((element) => {
+        return element.id !== id;
+    });
+    setItems(filterItems);
+};*/
 
-    const handleClick = () => {
-        if (!input) {
-            createRef.current.innerHTML = "Please enter input field";
-        } else if (input && !toggleSubmit) {
-            createRef.current.innerHTML = '';
-            setItems(
-                items.map((element) => {
-                    if (element.id === isEdit) {
-                        return {
-                            ...element,
-                            text: input,
-                        };
-                    }
-                    return element
-                })
-            );
-            setToggleSubmit(true);
-            setInput('');
-            setIsEdit(null);
-        } else {
-            createRef.current.innerHTML = '';
-            const allInput = {
-                id: Math.floor(Math.random() * 10000),
-                text: input,
-            };
-            setItems([...items, allInput]);
-            setInput('');
+const TodoItems = ({todo}) => {
+
+    const columns: GridColDef[] =
+        [
+            {field: 'col1', headerName: 'Id', width: 150},
+            {field: 'col2', headerName: 'Title', width: 150},
+            {field: 'col3', headerName: 'Status', width: 150},
+            {
+                field: "Delete",
+                renderCell: (cellValues) => {
+                    return (
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            /*onClick={(event) => {
+                                handleClick(event, cellValues);
+                            }}*/
+                        >
+                            Delete
+                        </Button>
+                    );
+                }
+            },
+        ];
+
+    const rows: GridRowsProp = todo.map((item, i) => (
+        {
+            id: i,
+            col1: item.id,
+            col2: item.title,
+            col3: item.completed ? 'Completed' : 'Incomplete',
         }
-    };
-
-    const editItem = (id) => {
-        const updateItem = items.find((element) => {
-            return element.id === id;
-        });
-
-        setToggleSubmit(false);
-        setInput(updateItem.text);
-        setIsEdit(id);
-    };
-
-    const deleteItem = (id) => {
-        const filterItems = items.filter((element) => {
-            return element.id !== id;
-        });
-        setItems(filterItems);
-    };
-
-    const removeItems = () => {
-        setItems([])
-    };
-
-    useEffect(() => {
-        localStorage.setItem('lists', JSON.stringify(items));
-    }, [items]);
-
+    ))
     return (
-        <div className="container">
-            {/*<div className="todoImage">
-                <Zoom direction="right" delay="100" triggerOnce>
-                    <TodoCalender />
-                </Zoom>
-                <img src={headerImage} alt="header-pic" width="100%" height="150px" />
-            </div>*/}
-                <div className="todo_main">
-                    <span className="msg" ref={createRef}></span>
-                    <input
-                        type="text"
-                        name="input"
-                        placeholder="Add Items"
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        className="todo-input"
-                    />
-
-                    {toggleSubmit ? (
-                        <i
-                            class="fas fa-plus-circle todo-icons todo-icons-add"
-                            onClick={handleClick}
-                            title="Add Item"
-                        >add</i>
-                    ) : (
-                        <i
-                            class="fas fa-edit todo-icons todo-icons-edit"
-                            onClick={handleClick}
-                            title="Edit Item"
-                        >edit</i>
-                    )}
-
-                    <div className="showItem">
-                        {items.map((item) => {
-                            return (
-
-                                <div className="todo-row">
-                                    <h3>{item.text}</h3>
-                                    <div className="icons_right">
-                                        <i
-                                            class="far fa-edit icons"
-                                            title="Edit Item"
-                                            onClick={() => editItem(item.id)}
-                                        >edit1</i>
-                                        <i
-                                            class="far fa-trash-alt icons"
-                                            title="Delete Item"
-                                            onClick={() => deleteItem(item.id)}
-                                        >delete</i>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-
-                    <div className="showItem">
-                        <button className="todo-button" onClick={removeItems}>
-                            clear all
-                        </button>
-                    </div>
-                </div>
+        <div style={{height: 300, width: '100%'}}>
+            <DataGrid rows={rows} columns={columns}/>
         </div>
     );
 }
